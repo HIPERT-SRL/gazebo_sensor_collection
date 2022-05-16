6170845bcd495d003e48c6ee00659fddbd5cf67c
mbosi <massimiliano.bosi@hipert.it>
2022-05-16 09:53:18 +0200

    update package

--- include/gazebo_sensor_collection/GazeboRosLidar.h
+++ include/gazebo_sensor_collection/GazeboRosLidar.h
@@ -43,47 +43,49 @@ namespace gazebo
   {
     /// \brief Constructor
     /// \param parent The parent entity, must be a Model or a Sensor
-    public: GazeboRosLidar();
+  public:
+    GazeboRosLidar();
 
     /// \brief Destructor
-    public: ~GazeboRosLidar();
+    ~GazeboRosLidar();
 
     /// \brief Load the plugin
     /// \param take in SDF root element
-    public: void Load(sensors::SensorPtr _parent, sdf::ElementPtr _sdf);
+    void Load(sensors::SensorPtr _parent, sdf::ElementPtr _sdf);
 
     /// \brief Subscribe on-demand
-    private: void ConnectCb();
+  private:
+    void ConnectCb();
 
     /// \brief The parent ray sensor
-    private: sensors::RaySensorPtr parent_ray_sensor_;
+    sensors::RaySensorPtr parent_ray_sensor_;
 
     /// \brief Pointer to ROS node
-    private: ros::NodeHandle* nh_;
+    ros::NodeHandle *nh_;
 
     /// \brief ROS publisher
-    private: ros::Publisher pub_;
+    ros::Publisher pub_;
 
     /// \brief topic name
-    private: std::string topic_name_;
+    std::string topic_name_;
 
     /// \brief frame transform name, should match link name
-    private: std::string frame_name_;
+    std::string frame_name_;
 
     /// \brief Minimum range to publish
-    private: double min_range_;
+    double min_range_;
 
     /// \brief Maximum range to publish
-    private: double max_range_;
+    double max_range_;
 
     /// \brief Update rate
-    private: double update_rate_;
+    double update_rate_;
 
     /// \brief Gaussian noise
-    private: double gaussian_noise_;
+    double gaussian_noise_;
 
     /// \brief Gaussian noise generator
-    private: static double gaussianKernel(double mu, double sigma)
+    static double gaussianKernel(double mu, double sigma)
     {
       // using Box-Muller transform to generate two independent standard normally distributed normal variables
       // see wikipedia
@@ -93,21 +95,20 @@ namespace gazebo
     }
 
     /// \brief A mutex to lock access
-    private: boost::mutex lock_;
+    boost::mutex lock_;
 
     /// \brief For setting ROS name space
-    private: std::string robot_namespace_;
+    std::string robot_namespace_;
 
     // Custom Callback Queue
-    private: ros::CallbackQueue laser_queue_;
-    private: void laserQueueThread();
-    private: boost::thread callback_laser_queue_thread_;
+    ros::CallbackQueue laser_queue_;
+    void laserQueueThread();
+    boost::thread callback_laser_queue_thread_;
 
     // Subscribe to gazebo laserscan
-    private: gazebo::transport::NodePtr gazebo_node_;
-    private: gazebo::transport::SubscriberPtr sub_;
-    private: void OnScan(const ConstLaserScanStampedPtr &_msg);
-
+    gazebo::transport::NodePtr gazebo_node_;
+    gazebo::transport::SubscriberPtr sub_;
+    void OnScan(const ConstLaserScanStampedPtr &_msg);
   };
 
 } // namespace gazebo
